80a96f4ccbf41416009fcfac5caa02a331f69882
abbasalit987 <abbasalit987@gmail.com>
2023-04-14 07:20:41 +0530

    commit 11/ adding one to many relationship

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Gtr@vels123', {
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
   host: 'localhost'
 });
